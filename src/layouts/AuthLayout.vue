f78e172f6786f0497bc7a12f88a72a8b69baf2b3
Darwisy <bahijdarwisyfarras@gmail.com>
2026-02-23 17:22:02 +0700

    layout changes

---
+++ src/layouts/AuthLayout.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="min-h-screen p-5">
+    <RouterView />
+  </div>
+</template>
+<script setup></script>
--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,8 +1,11 @@
 <template>
   <PageTitle title="Dashboard" />
-  <DashboardLayout />
+  <div class="max-w-11/12 p-3 mx-auto bg-primary rounded-lg grid grid-cols-3">
+    <DashboardCard />
+  </div>
 </template>
 <script setup>
+import DashboardCard from "../components/DashboardCard.vue";
 import PageTitle from "../components/PageTitle.vue";
-import DashboardLayout from "../layouts/DashboardLayout.vue";
 </script>
+  
